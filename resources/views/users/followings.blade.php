7fc2fb261fd7a73a3d467aa9065bfc953a879c77
SHO <shlesson7@gmail.com>
2021-06-13 10:26:58 +0000

    follow, unfollow

--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -1,5 +1,3 @@
-
-
 @extends('layouts.app')
 
 @section('content')
@@ -11,12 +9,8 @@
         <div class="col-sm-8">
             {{-- タブ --}}
             @include('users.navtabs')
-            @if (Auth::id() == $user->id)
-                {{-- 投稿フォーム --}}
-                @include('microposts.form')
-            @endif
-            {{-- 投稿一覧 --}}
-            @include('microposts.microposts')
+            {{-- ユーザ一覧 --}}
+            @include('users.users')
         </div>
     </div>
 @endsection
